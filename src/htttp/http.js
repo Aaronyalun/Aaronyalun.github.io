c283ca67c7ecd7b231dedf33f68c0699d75dc4fd
Wang ZhuangZhuang <2318347536@qq.com>
2020-12-20 17:19:53 +0800

    修改以前版本

--- src/htttp/http.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-var httpIns= axios.create({
-  baseURL:"https://api.github.com",
-  timeout:30000,
-})
-export default httpIns
--- src/store/state.js
+++
@@ -1,7 +0,0 @@
-export default {
-  userName:"",
-  users:[], //用户列表
-  showText:true,//是否显示输入提示文案  优先级最高
-  showLoading:true,//是否显示输入提示文案 优先级其次
-  showNoBody:true//是否显示输入提示文案   优先级最低
-}
